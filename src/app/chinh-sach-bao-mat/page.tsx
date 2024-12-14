2044f915ca744178213f211954c10f9d8377d753
trinhkhang123 <khangmau12k@gmail.com>
2024-12-14 15:52:41 +0700

    update

---
+++ src/app/chinh-sach-bao-mat/page.tsx
@@ -0,0 +1,101 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Medusa Next.js Starter Template",
+  description:
+    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+}
+
+export default async function Chung() {
+  
+  return (
+    <>
+      <div className="py-[36px] px-[120px]">
+        <div className="flex flex-col gap-x-6 text-[32px]">
+          Chính Sách Bảo Mật Thông Tin
+        </div>
+
+        <div className="py-[16px] text-[20px] font-medium">
+        1. Mục đích và phạm vi thu thập
+        </div>
+        <div className="text-[16px]">
+  Comem.vn không bán, chia sẻ hay trao đổi thông tin cá nhân của khách hàng thu thập trên trang web cho một bên thứ ba nào khác.<br />
+  Thông tin cá nhân thu thập được sẽ chỉ được sử dụng trong nội bộ công ty.<br />
+  Khi bạn liên hệ đăng ký dịch vụ, thông tin cá nhân mà Comem.vn thu thập bao gồm:<br />
+  Họ và tên<br />
+  Địa chỉ<br />
+  Điện thoại<br />
+  Email<br />
+  Ngoài thông tin cá nhân là các thông tin về dịch vụ:<br />
+  Tên sản phẩm<br />
+  Số lượng<br />
+  Thời gian giao nhận sản phẩm
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+2. Phạm vi sử dụng thông tin
+        </div>
+        <div className="text-[16px]">
+        Thông tin cá nhân thu thập được sẽ chỉ được Comem.vn sử dụng trong nội bộ công ty và cho một hoặc tất cả các mục đích sau đây:<br />
+        - Hỗ trợ khách hàng <br />
+        - Cung cấp thông tin liên quan đến dịch vụ<br />
+        - Xử lý đơn đặt hàng và cung cấp dịch vụ và thông tin qua trang web của chúng tôi theo yêu cầu của bạn<br />
+        - Chúng tôi có thể sẽ gửi thông tin sản phẩm, dịch vụ mới, thông tin về các sự kiện sắp tới hoặc thông tin tuyển dụng nếu quý khách đăng kí nhận email thông báo.<br />
+        - Ngoài ra, chúng tôi sẽ sử dụng thông tin bạn cung cấp để hỗ trợ quản lý tài khoản khách hàng; xác nhận và thực hiện các giao dịch tài chính liên quan đến các khoản thanh toán trực tuyến của bạn.<br />
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+3. Thời gian lưu trữ thông tin
+        </div>
+        <div className="text-[16px]">
+        Đối với thông tin cá nhân, Comem.vn chỉ xóa đi dữ liệu này nếu khách hàng có yêu cầu, khách hàng yêu cầu gửi mail về cskh@comem.vn<br />
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+4. Những người hoặc tổ chức có thể được tiếp cận với thông tin cá nhân
+        </div>
+        <div className="text-[16px]">
+        Đối với thông tin cá nhân, Comem.vn chỉ xóa đi dữ liệu này nếu khách hàng có yêu cầu, khách hàng yêu cầu gửi mail về cskh@comem.vn<br />
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+3. Thời gian lưu trữ thông tin
+        </div>
+        <div className="text-[16px]">
+        Đối với thông tin cá nhân, Comem.vn chỉ xóa đi dữ liệu này nếu khách hàng có yêu cầu, khách hàng yêu cầu gửi mail về cskh@comem.vn<br />
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+4. Những người hoặc tổ chức có thể được tiếp cận với thông tin cá nhân
+        </div>
+        <div className="text-[16px]">
+        Đối tượng được tiếp cận với thông tin cá nhân của khách hàng thuộc một trong những trường hợp sau:<br />
+        <div className="py-[16px]">- CÔNG TY CP MỸ PHẨM THIÊN NHIÊN CỎ MỀM <br /></div>
+        - Các đối tác có ký hợp động thực hiện 1 phần dịch vụ do CÔNG TY CP MỸ PHẨM THIÊN NHIÊN CỎ MỀM. Các đối tác này sẽ nhận được những thông tin theo thỏa thuận hợp đồng (có thể 1 phần hoặc toàn bộ thông tin tuy theo điều khoản hợp đồng) để tiến hành hỗ trợ người dùng sử dụng dịch vụ do Công ty cung cấp.<br />
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+5. Địa chỉ của đơn vị thu thập và quản lý thông tin cá nhân
+        </div>
+        <ul className="list-disc text-[16px] pl-6">
+        <li>CÔNG TY CP MỸ PHẨM THIÊN NHIÊN CỎ MỀM</li>
+        <li> Địa chỉ: Số 225, Trần Đăng Ninh, P. Dịch Vọng, Q. Cầu Giấy, TP. Hà Nội.</li>
+        <li> Điện thoại: 1800646890.</li>
+        <li> Website: Comem.vn</li>
+        <li> Email: cskh@comem.vn</li>
+</ul>
+<div className="py-[16px] text-[20px] font-medium">
+6. Phương tiện và công cụ để người dùng tiếp cập và chỉnh sử dữ liệu cá nhân của mình        </div>
+        <div className="text-[16px]">
+        Comem.vn không thu thập thông tin khách hàng qua trang web, thông tin cá nhân khách hàng được thực hiện thu thập qua email liên hệ đặt mua sản phẩm, dịch vụ gửi về hộp mail của chúng tôi: cskh@comem.vn hoặc số điện thoại liên hệ đặt mua sản phẩm gọi về 1800646890<br />
+        <div className="pt-[16px]">Bạn có thể liên hệ địa chỉ email cùng số điện thoại trên để yêu cầu Comem.vn chỉnh sửa dữ liệu cá nhân của mình <br /></div>
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+7. Cơ chế tiếp nhận và giải quyết khiếu nại của người tiêu dùng liên quan đến việc thông tin cá nhân bị sử dụng sai mục đích hoặc phạm vi đã thông báo      </div>
+        <div className="text-[16px]">
+        Tại Comem.vn , việc bảo vệ thông tin cá nhân của bạn là rất quan trọng, bạn được đảm bảo rằng thông tin cung cấp cho chúng tôi sẽ được mật Comem.vn cam kết không chia sẻ, bán hoặc cho thuê thông tin cá nhân của bạn cho bất kỳ người nào khác. Comem.vn cam kết chỉ sử dụng các thông tin của bạn vào các trường hợp sau:<br />
+        <div className="pt-[16px]">- Nâng cao chất lượng dịch vụ dành cho khách hàng<br /></div>
+        <div className="pt-[16px]">- Giải quyết các tranh chấp, khiếu nại<br /></div>
+
+        <div className="pt-[16px]">- Khi cơ quan pháp luật có yêu cầu.<br /></div>
+        <div className="pt-[16px]">Comem.vn hiểu rằng quyền lợi của bạn trong việc bảo vệ thông tin cá nhân cũng chính là trách nhiệm của chúng tôi nên trong bất kỳ trường hợp có thắc mắc, góp ý nào liên quan đến chính sách bảo mật của Comem.vn , và liên quan đến việc thông tin cá nhân bị sử dụng sai mục đích hoặc phạm vi đã thông báo vui lòng liên hệ qua số hotline 1800646890 hoặc email: cskh@comem.vn<br /></div>
+
+</div>
+      </div>
+    </>
+  )
+}
---
+++ src/app/chinh-sach-doi-tra-hoan-tien/page.tsx
@@ -0,0 +1,72 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Medusa Next.js Starter Template",
+  description:
+    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+}
+
+export default async function Chung() {
+  
+  return (
+    <>
+      <div className="py-[36px] px-[120px]">
+        <div className="flex flex-col gap-x-6 text-[32px]">
+        Chính Sách Đổi Trả Và Hoàn Tiền
+        </div>
+
+        <div className="py-[16px] text-[20px] font-medium">
+        1. Đổi trả do lỗi sai đơn hoặc lỗi vận chuyển:
+        </div>
+        <div className="text-[16px]">
+        Để đảm bảo chất lượng dịch vụ và đem lại cho Quý Khách những trải nghiệm tốt nhất với sản phẩm, Cỏ Mềm áp dụng chính sách đổi hàng với các trường hợp sau:<br />
+  <div className="py-[16px] pl-[16px]">
+  1. Nhầm sản phẩm, thiếu hoặc thừa sản phẩm so với đơn hàng đã xác nhận<br />
+  2. Sản phẩm bị móp vỡ, gãy hỏng do vận chuyển<br />
+  3. Sản phẩm có lỗi kỹ thuật phát hiện khi sử dụng<br />
+  </div>
+  <div className="pt-[16px]">Trong 3 trường hợp trên, Cỏ Mềm đổi hàng cho quý khách theo trình tự sau:<br /></div>
+  <div className="pt-[16px]">Bước 1. Quý khách vui lòng thông báo cho Cỏ Mềm trong vòng 24h kể từ khi nhận hàng, kèm theo ảnh chụp gói hàng và toàn bộ sản phẩm nhận được<br /></div>
+  <div className="pt-[16px]">Bước 2. Cỏ Mềm tiến hành gửi mặt hàng thiếu/lỗi, hỏng đúng số lượng và chủng loại cho quý khách trong vòng 5 ngày từ khi nhận được yêu cầu<br /></div>
+  <div className="pt-[16px]">Bước 3. Quý khách gửi lại mặt hàng thừa/lỗi, hỏng về địa chỉ công ty: số 225 phố Trần Đăng Ninh, phường Dịch Vọng, quân Cầu Giấy, Hà Nội<br /></div>
+  <div className="pt-[16px]">Bước 4. Trong trường hợp có phát sinh chênh lệch, Cỏ Mềm chuyển khoản cho quý khách vào số tài khoản ngân hàng do quý khách cung cấp hoặc quý khách thanh toán phần chênh lệch cho Cỏ Mềm khi nhận được hàng đổi<br /></div>
+
+  <div className="pt-[16px]">Phí vận chuyển đổi trả: với sản phẩm cần đổi do 03 lý do trên, quý khách không phải trả phí vận chuyển, mọi chi phí vận chuyển phát sinh sẽ do Cỏ Mềm thanh toán bằng hình thức thanh toán trực tiếp cho đơn vị vận chuyển.<br /></div>
+
+</div>
+<div className="py-[16px] text-[20px] font-medium">
+2. Đổi trả sản phẩm theo mong muốn riêng của khách hàng: 
+        </div>
+        <div className="text-[16px]">
+        <div className="pt-[16px]">Với mong muốn Khách lựa chọn được sản phẩm tốt và có trải nghiệm hài lòng nhất, cũng như đảm bảo niềm tin vào sản phẩm Cỏ Mềm, khách hàng có thể đổi trả sản phẩm đã mua tại Cỏ Mềm trong vòng 05 ngày kể từ khi nhận hàng.<br /></div>
+        <div className="pt-[16px]">Điều kiện đổi trả:<br /></div>
+        <ul className="pt-[16px] pl-[24px] list-disc">
+  <li>Sản phẩm chưa qua sử dụng, còn nguyên bao bì đóng gói, đầy đủ phụ kiện nếu có.<br /></li>
+  <li> Bao bì sản phẩm không bị bẩn, méo vỡ, có mùi lạ hoặc có dấu hiệu bảo quản không đúng với hướng dẫn sử dụng.<br /></li>
+  <li>  Còn hoá đơn mua hàng.<br /></li>
+  </ul>
+  <div className="pt-[16px]">Lưu ý:<br /></div>
+  <ul className="list-disc pt-[16px] pl-[24px]">
+  <li>Sản phẩm chỉ được đổi 01 lần.<br /></li>
+  <li> Quý khách vui lòng thanh toán phí vận chuyển 2 chiều khi yêu cầu đổi trả hàng theo mong muốn. <br /></li>
+  <li>  Trong trường hợp có phát sinh chênh lệch, Cỏ Mềm chuyển khoản cho quý khách vào số tài khoản ngân hàng do quý khách cung cấp hoặc quý khách thanh toán cho Cỏ Mềm khi nhận được hàng đổi<br /></li>
+  </ul>
+  <div className="pt-[16px]">3. Hoàn trả sản phẩm:<br /></div>
+  <div className="pt-[16px]">Trong trường hợp khách hàng gặp phản ứng không mong muốn với sản phẩm. Quý khách vui lòng cung cấp hình ảnh về tình trạng gặp phải hoặc giấy khám sức khỏe từ cơ sở y tế để Cỏ Mềm xác nhận.<br /></div>
+  <ul className="list-disc pt-[16px] pl-[24px]">
+  <li>Cách thức hoàn trả:<br /></li>
+  </ul>
+  Bước 1: Quý khách gửi hàng về cho Cỏ Mềm theo quy định (Phí vận chuyển sẽ do Cỏ Mềm thanh toán)<br />
+  <div className="pt-[16px]">Bước 2: Quý khách cung cấp Số tài khoản của quý khách bằng cách Inbox cho Cỏ Mềm tại: http://m.me/comemhomelab<br /></div>
+  <div className="pt-[16px]">Bước 3: Cỏ Mềm hoàn tiền bằng hình thức chuyển khoản cho quý khách trong vòng 5 ngày làm việc (Không bao gồm ngày nghỉ, lễ tết).<br /></div>
+  <div className="pt-[16px]">Gửi hàng về Cỏ Mềm:<br /></div>
+
+  Trong các trường hợp Bảo hành hoặc Đổi trả có yêu cầu gửi hàng về Cỏ Mềm, Quý khách vui lòng đóng gói sản phẩm cẩn thận nhằm đảm bảo trạng thái nguyên vẹn và gửi hàng cho đối tác vận chuyển trong vòng 48h kể từ khi hoàn thành việc trao đổi xác nhận thông tin với Cỏ Mềm về đơn hàng. Cỏ Mềm rất tiếc sẽ phải từ chối sản phẩm bị hư hại do lỗi đóng gói sơ sài từ Quý khách. <br />
+  <div className="pt-[16px]">Địa chỉ nhận hàng đổi trả: số 255 phố Trần Đăng Ninh, phường Dịch Vọng, quận Cầu Giấy, Hà Nội<br /></div>
+
+</div>
+
+      </div>
+    </>
+  )
+}
